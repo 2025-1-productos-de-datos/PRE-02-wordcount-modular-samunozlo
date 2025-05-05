f99c5058f582d657911dfada61a3e6cc4e35b855
samunozlo <samunozlo@unal.edu.co>
2025-05-04 21:17:48 -0500

    Update write_word_counts.py

--- homework/src/_internals/write_word_counts.py
+++ homework/src/_internals/write_word_counts.py
@@ -6,7 +6,7 @@ def write_word_counts(counter,output_folder):
         os.makedirs(output_folder)
 
     # Guardar los resultados
-    output_path=os.path.join(output_folder,"results.tsv")
+    output_path=os.path.join(output_folder,"wordcount.tsv")
     with open(output_path, "w", encoding="utf-8") as f:
         for key, value in counter.items():
             f.write(f"{key}\t{value}\n")
